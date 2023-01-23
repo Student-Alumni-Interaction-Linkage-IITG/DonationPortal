e575960dc0c881de820a6662490ac8269377df88
Sayan Dey Sarkar <sayandeysarkar2003@gmail.com>
2023-01-24 03:41:02 +0530

    minor changes

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,11 @@ import Home from './Pages/Home';
 import Projects from './Pages/projects-page/projects';
 import Projectdetail from './Pages/projectdetailspage/projectdetail';
 import MyProfile from './Pages/profile/myProfile';
-updates
+
 import Gsproject_putiing from './Pages/gsdashboard/Gsproject_putiing';
 import Login from './Pages/login/login';
 import Signup from './Pages/login/signup';
-updates
+
 function App() {
   return (
     <Router>
@@ -29,7 +29,7 @@ function App() {
             <Route exact path="/projectdetail.js">
               <Projectdetail/>
             </Route>
-updates
+
             <Route exact path="/login.js">
               <Login/>
             </Route>
@@ -39,7 +39,7 @@ updates
             <Route exact path="/abcd">
               <Gsproject_putiing/>
             </Route>
-updates
+
           </Switch>
         </div>
       </div>
